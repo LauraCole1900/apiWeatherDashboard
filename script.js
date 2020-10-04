49948d9b62f05e07245af1195a59a61420fe80ff
Laura Cole <lauracole1900@comcast.net>
2020-10-03 22:24:27 -0600

    tried to add uv data, does not work

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ $("#find-location").on("click", function (e) {
 
 
   // API query url
+  var city = $("#city").val();
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=2278d7ef6a3b88793ffca205108a944e";
 
 
@@ -14,7 +15,7 @@ $("#find-location").on("click", function (e) {
   // AJAX call
   // create table rows
   // put data into table
-  // weather icons for current conditions
+  // use weather icons for current conditions
   $.ajax({
     url: queryURL,
     method: "GET"
@@ -25,12 +26,20 @@ $("#find-location").on("click", function (e) {
     var tempTd = $("<td>").text(response.main.temp);
     var humTd = $("<td>").text(response.main.humidity);
     var windTd = $("<td>").text(response.wind.speed);
-    // var uvTd = $("<td>").text(response.);
+
+    // information needed for UV Index
+    var lat = response.coord.lat;
+    var lon = response.coord.lon;
+    var uvData = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=2278d7ef6a3b88793ffca205108a944e";
+    var uvIndex = JSON.parse(uvData) // something wrong here
+    var uvTd = $("<td>").text(uvIndex.value);
 
     // add data to table row
-    tRow.prepend(cityTd, currTd, tempTd, humTd, windTd, uvTd)
+    tRow.append(cityTd, currTd, tempTd, humTd, windTd, uvTd)
 
     // add table row to table body
-    $("tbody").append(tRow);
+    $("tbody").prepend(tRow);
   });
 });
+
+// uv index 0-2 = low, 3-5 = moderate, 6-7 = high, 8-10 = very high, 11+ = extreme
