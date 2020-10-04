104f214d1263d7c600d11421ab3f5c54ad9e8260
Laura Cole <lauracole1900@comcast.net>
2020-10-04 10:53:01 -0600

    basic functionality works, no forecast yet

--- README.md
+++ README.md
@@ -1,27 +1,24 @@
 # Weather Dashboard
 
-#### Description
+### Description
 
 A weather dashboard using a third-party API
 
 
-#### Screenshots
-Blank scheduler:
-![Scheduler](assets/scheduler-screencap.png)
+### Screenshots
+Blank dashboard:
+![Dashboard](assets/scheduler-screencap.png)
 
-User input form:
+Dashboard with cities entered:
 ![User input form](assets/input-form.png)
 
-Scheduler with event scheduled:
-![Scheduled event](assets/scheduled-event.png)
 
-
-#### Link
-[jQuery workday scheduler](https://lauracole1900.github.io/apiWeatherDashboard/)
+### Link
+[Weather Dashboard](https://lauracole1900.github.io/apiWeatherDashboard/)
 
 
 
-#### Licenses
+### Licenses
 
-Weather icons created by Joey Yakimowich-Payne
+Weather icons in the header created by Joey Yakimowich-Payne
   licensed under Creative Commons (http://creativecommons.org/licenses/by/3.0/)
--- script.js
+++ script.js
@@ -8,8 +8,10 @@ $("#find-location").on("click", function (e) {
 
   // API query url
   var city = $("#city").val();
+  callForecast(city);
   var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=2278d7ef6a3b88793ffca205108a944e";
 
+  var tRow = $("<tr>");
 
 
   // AJAX call
@@ -20,26 +22,59 @@ $("#find-location").on("click", function (e) {
     url: queryURL,
     method: "GET"
   }).then(function (response) {
-    var tRow = $("<tr>");
     var cityTd = $("<td>").text(response.name);
-    var currTd = $("<td>").text(response.weather.icon);
+    var weatherIcon = $("<img>").attr("src", `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`)
+    var currTd = $("<td>").append(weatherIcon);
     var tempTd = $("<td>").text(response.main.temp);
     var humTd = $("<td>").text(response.main.humidity);
     var windTd = $("<td>").text(response.wind.speed);
-
-    // information needed for UV Index
+    tRow.append(cityTd, currTd, tempTd, humTd, windTd)
     var lat = response.coord.lat;
     var lon = response.coord.lon;
+    setTimeout(function () {
+      getUv(lat, lon)
+    }, 100)
+  })
+
+  function getUv(lat, lon) {
     var uvData = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=2278d7ef6a3b88793ffca205108a944e";
-    var uvIndex = JSON.parse(uvData) // something wrong here
-    var uvTd = $("<td>").text(uvIndex.value);
 
-    // add data to table row
-    tRow.append(cityTd, currTd, tempTd, humTd, windTd, uvTd)
+    $.ajax({
+      url: uvData,
+      method: "GET"
+    }).then(function (response) {
+      var uvTd = $("<td>").text(response.value);
+      tRow.append(uvTd)
+      if (response.value <= 2) {
+        uvTd.addClass("low")
+      } else if (response.value > 2 && response.value <= 5) {
+        uvTd.addClass("moderate")
+      } else if (response.value > 6 && response.value <= 7) {
+        uvTd.addClass("high")
+      } else if (response.value > 7 && response.value <= 10) {
+        uvTd.addClass("very_high")
+      } else if (response.value > 10) {
+        uvTd.addClass("extreme")
+      }
 
-    // add table row to table body
-    $("tbody").prepend(tRow);
-  });
+    })
+  }
+
+  // add table row to table body
+  $("tbody").prepend(tRow);
 });
+
+
+function callForecast(city) {
+  var forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=2278d7ef6a3b88793ffca205108a944e`;
+
+  $.ajax({
+    url: forecast,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
+  })
+  // for loop, i+8
+}
 
 // uv index 0-2 = low, 3-5 = moderate, 6-7 = high, 8-10 = very high, 11+ = extreme
